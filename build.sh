7bed8d0b2a78c49a4cfdbbd43d12625633d29681
eakyoungyou <eakyoungyu@gmail.com>
2023-01-25 00:17:45 +0900

    add shell commands to update poetry.lock and pip

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 # exit on error
 set -o errexit
 
+pip install --upgrade pip
+poetry update --lock
 poetry install
+pip install --force-reinstall -U setuptools
 
 python manage.py collectstatic --no-input
 python manage.py migrate

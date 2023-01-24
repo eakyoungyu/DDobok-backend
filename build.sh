eceb6a98a23a2327a64bae81c4f32505f446d837
eakyoungyou <eakyoungyu@gmail.com>
2023-01-25 01:16:32 +0900

    regenerate poetry.lock in build.sh

--- build.sh
+++ build.sh
@@ -4,12 +4,9 @@ set -o errexit
 
 pip install --upgrade pip
 poetry --version
-echo "update poetry"
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 - --uninstall
-curl -sSL https://install.python-poetry.org | python3 -
-# poetry self update
-# echo "update poetry.lock"
-# poetry lock
+echo "regenerate poetry.lock"
+rm poetry.lock
+poetry lock
 echo "before install poetry"
 poetry install
 pip install --force-reinstall -U setuptools

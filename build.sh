f5dbdc4dfa243b1a0248dc10c43e9ea8f87146dc
eakyoungyou <eakyoungyu@gmail.com>
2023-02-07 21:09:20 +0900

    remove user creation

--- build.sh
+++ build.sh
@@ -13,4 +13,3 @@ pip install --force-reinstall -U setuptools
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.filter(email='y2k2@gmail.com').delete(); User.objects.create_superuser('y2k2@gmail.com', 'y2k2', '1234')" | python manage.py shell
